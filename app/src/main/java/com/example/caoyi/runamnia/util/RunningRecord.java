b15b5aba92f237156892c8e66748ec79ec5ca383
neaboo822 <ss1234534@hotmail.com>
2015-05-16 16:33:51 -0500

    Added Record, RunningRecord class

---
+++ app/src/main/java/com/example/caoyi/runamnia/util/RunningRecord.java
@@ -0,0 +1,16 @@
+package com.example.caoyi.runamnia.util;
+
+import java.util.*;
+
+/**
+ * Created by Chih-YenChang on 5/16/15.
+ */
+public class RunningRecord {
+    private Date runStart;
+    private Date runEnd;
+    private double calories;
+    private double distance;
+    private double weight;
+    private double height;
+
+}
