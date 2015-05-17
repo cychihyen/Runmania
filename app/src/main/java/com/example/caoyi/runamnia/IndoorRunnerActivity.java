b05506ac85e32e18e2bb53e924752c7c499eb77c
neaboo822 <ss1234534@hotmail.com>
2015-05-17 09:46:38 -0500

    part of distance calculation

--- app/src/main/java/com/example/caoyi/runamnia/IndoorRunnerActivity.java
+++ app/src/main/java/com/example/caoyi/runamnia/IndoorRunnerActivity.java
@@ -22,7 +22,9 @@ public class IndoorRunnerActivity extends ActionBarActivity implements SensorEve
     private double count;
     boolean activityRunning;
 
-
+    private double tempDistance = 0;
+    private double tempTimestamp = 0;
+    Record userRecord;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,6 +36,7 @@ public class IndoorRunnerActivity extends ActionBarActivity implements SensorEve
         setContentView(R.layout.activity_indoor_runner);
         sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
         activityRunning = true;
+        Date startTime;
 
     try {
         UserJsonReader userJsonReader = new UserJsonReader();
@@ -41,11 +44,15 @@ public class IndoorRunnerActivity extends ActionBarActivity implements SensorEve
         /**
          * Get start time.
          */
-        Date startTime = new Date();
+        startTime = new Date();
 
-        Record userRecord = userJsonReader.readFromFile();
+        userRecord = userJsonReader.readFromFile();
         Sensor countSensor = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER);
-
+        if (countSensor != null) {
+            sensorManager.registerListener(this, countSensor, SensorManager.SENSOR_DELAY_UI);
+        } else {
+            Toast.makeText(this, "Count sensor not available!", Toast.LENGTH_LONG).show();
+        }
         /**
          * loop sensor updates
          */
@@ -75,20 +82,16 @@ public class IndoorRunnerActivity extends ActionBarActivity implements SensorEve
 
     synchronized private void updateLoop(Sensor countSensor){
         while(activityRunning){
-            if (countSensor != null) {
-                sensorManager.registerListener(this, countSensor, SensorManager.SENSOR_DELAY_UI);
-            } else {
-                Toast.makeText(this, "Count sensor not available!", Toast.LENGTH_LONG).show();
-            }
-
-               try {
-                   Thread.sleep(500); //Constant
-               } catch (Exception e) {
-
-               }
-
+            runmania.sendInstantDistance(distance);
+            String usersSpeeds runmania.requestFromServer();
             // send data to server
             // update view using tmp Class
+            try {
+                   Thread.sleep(500); //Constant
+            } catch (Exception e) {
+
+            }
+
 
         }
     }
@@ -96,8 +99,11 @@ public class IndoorRunnerActivity extends ActionBarActivity implements SensorEve
     @Override
     public void onSensorChanged(SensorEvent event){
         if(activityRunning){
-            int stepCOunt = (int) event.values[0];
-            // Distance
+            int stepCount = (int) event.values[0];
+            double height = userRecord.getHeight();
+            tempDistance = stepCount*(height/0.8);
+            tempTimestamp = new Date() - startTime;
+
             // update tmp Class
 
         }
