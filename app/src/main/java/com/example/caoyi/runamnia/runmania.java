019a3dc67242ae4f11f256da283e20539201bb0f
SidW <wu.xiangxiang@gmail.com>
2015-05-17 11:50:30 -0500

    socket done

--- app/src/main/java/com/example/caoyi/runamnia/runmania.java
+++ app/src/main/java/com/example/caoyi/runamnia/runmania.java
@@ -11,8 +11,46 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import com.example.caoyi.runamnia.util.SystemUiHider;
 
-public class runmania extends ListActivity {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Scanner;
+
+
+public class runmania extends Activity {
+    static  BufferedReader in;
+    static PrintWriter out;
+    static Socket socket;
+
+    public static ClientThread client;
+
+    public class ClientThread implements Runnable{
+        public void run(){
+            try{
+                runmania.socket = new Socket("192.168.3.252", 9898);
+                runmania.in = new BufferedReader(
+                        new InputStreamReader(runmania.socket.getInputStream()));
+                runmania.out = new PrintWriter(runmania.socket.getOutputStream(), true);
+
+                // Consume the initial welcoming messages from the server
+                for (int i = 0; i < 2; i++) {
+                    System.out.println(runmania.in.readLine() + "\n");
+                }
+            }catch (IOException e){
+                System.out.println(e.getMessage());
+            }
+        }
+        public void createRoom(String roomName, String roomDis){
+            runmania.out.println("CreateRoom " + roomName + " " + roomDis);
+        }
+
+    }
+
+    private int distance;
 
     String[] countryArray = {"India", "Pakistan", "USA", "UK", "India", "Pakistan", "USA", "UK","India", "Pakistan", "USA", "UK"};
 
@@ -35,8 +73,10 @@ public class runmania extends ListActivity {
                 final String item = (String) parent.getItemAtPosition(position);
                 //Toast.makeText(this, item + " selected", Toast.LENGTH_LONG).show();
             }
-
         });
+
+        this.client = new ClientThread();
+        new Thread(client).start();
     }
 
     @Override
